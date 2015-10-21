09454df7e4e3c16edcb90c2e163d0df8292a8427
Satsuki Ueno <satsukiueno@gmail.com>
2015-10-21 14:23:36 -0500

    Added preliminary editing functionality

--- app/src/main/java/cs3354group10/messenger/activities/EditMessageActivity.java
+++ app/src/main/java/cs3354group10/messenger/activities/EditMessageActivity.java
@@ -2,8 +2,12 @@ package cs3354group10.messenger.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import group10.cs3354.sms_messenger.R;
 
@@ -35,5 +39,21 @@ public class EditMessageActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * onSendPressed
+     * sends the message
+     * @param view Not used
+     * TODO: need to add way for multiple contacts, send by ontact and not phone number
+     */
+    public void onSendPressed(View view){
+        String message = ((EditText) findViewById(R.id.id_message_field)).getText().toString();
+        String address = ((EditText) findViewById(R.id.id_phone_field)).getText().toString();
+
+        SmsManager manager = SmsManager.getDefault();
+        manager.sendTextMessage(address,null,message,null,null);
+
+        //TODO: probably switch activity or something, save to database, etc
     }
 }
