b9c56a6a63c692b9385ec2212721a2f352f6a46e
Justin Head <justin@cubedhost.com>
2015-10-28 13:18:47 -0500

    Update DB schema to match recent changes

--- app/src/main/java/cs3354group10/messenger/db/MessageDatabaseHelper.java
+++ app/src/main/java/cs3354group10/messenger/db/MessageDatabaseHelper.java
@@ -5,33 +5,42 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import cs3354group10.messenger.Message;
-import cs3354group10.messenger.MessageThread;
 
 public class MessageDatabaseHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_PATH = "Messenger.db";
 
-    public MessageDatabaseHelper(Context context) {
+    private static MessageDatabaseHelper instance;
+
+    private MessageDatabaseHelper(Context context) {
         super(context, DATABASE_PATH, null, DATABASE_VERSION);
+    }
+
+    /**
+     * MessageDatabaseHelper singleton
+     *
+     * @param context via getContext() within the activity
+     * @return an instance of MessageDatabaseHelper
+     */
+    public static MessageDatabaseHelper getInstance(Context context) {
+        if (instance == null) {
+            instance = new MessageDatabaseHelper(context);
+        }
+
+        return instance;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // Create threads table
-        db.execSQL(
-            "CREATE TABLE " + MessageThread.DB_TABLE_NAME + " (" +
-            MessageThread.DB_COLUMN_NAME_ID + " INTEGER PRIMARY KEY," +
-            MessageThread.DB_COLUMN_NAME_CONTACT + " VARCHAR(255)" +
-            ")"
-        );
-
         // Create messages table
         db.execSQL(
             "CREATE TABLE " + Message.DB_TABLE_NAME + " (" +
-            Message.DB_COLUMN_NAME_ID + " INTEGER PRIMARY KEY," +
-            Message.DB_COLUMN_NAME_CONTACT + " VARCHAR(255)" +
-            Message.DB_COLUMN_NAME_TEXT + " TEXT" +
+            Message.DB_COLUMN_NAME_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+            Message.DB_COLUMN_NAME_CONTACT + " VARCHAR(255)," +
+            Message.DB_COLUMN_NAME_TIMESTAMP + " TIMESTAMP," +
+            Message.DB_COLUMN_NAME_TEXT + " TEXT," +
+            Message.DB_COLUMN_NAME_STATE + " INT(1)" +
             ")"
         );
     }
