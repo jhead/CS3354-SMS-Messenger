29f7d60a3cb7aedea2ad52f9de5a3df88cf55710
doanian <brendanhonea@gmail.com>
2015-11-10 12:16:32 -0600

    added serach activity

---
+++ app/src/main/java/cs3354group10/messenger/activities/SearchActivity.java
@@ -0,0 +1,16 @@
+package cs3354group10.messenger.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import group10.cs3354.sms_messenger.R;
+
+public class SearchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+}
