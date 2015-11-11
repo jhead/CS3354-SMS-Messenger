0bf25de804bd5662eeb81d4ed4bcafbf6149c0d3
doanian <brendanhonea@gmail.com>
2015-11-10 20:24:29 -0600

    preliminary searching working

--- app/src/main/java/cs3354group10/messenger/activities/SearchActivity.java
+++ app/src/main/java/cs3354group10/messenger/activities/SearchActivity.java
@@ -1,5 +1,6 @@
 package cs3354group10.messenger.activities;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.app.Activity;
@@ -10,11 +11,18 @@ import android.app.ListActivity;
 import android.widget.ListAdapter;
 import android.widget.SimpleCursorAdapter;
 
+import cs3354group10.messenger.Contact;
+import cs3354group10.messenger.Message;
+import cs3354group10.messenger.MessageState;
+import cs3354group10.messenger.db.MessageDatabase;
 import group10.cs3354.sms_messenger.R;
 
 public class SearchActivity extends ListActivity {
 
     EditText inputText;
+    private ListAdapter listAdapter;
+    private String[] fromColumn = {Message.DB_COLUMN_NAME_TEXT};
+    private int[] toView = {R.id.searchResult};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +37,7 @@ public class SearchActivity extends ListActivity {
                 String str = inputText.getText().toString();
 
                 if(searchMessages(str)){
+
                 }
                 else{
 
@@ -39,8 +48,18 @@ public class SearchActivity extends ListActivity {
     }
 
     private boolean searchMessages(String searchStr){
-        
+        //DEBUG MESSAGES
 
+
+
+        Context context = getApplicationContext();
+        MessageDatabase.insertMessage(context, new Message(new Contact("Brendan"), "test", MessageState.RECV));
+        Cursor messageResultCursor = MessageDatabase.queryMessagesForString(context, searchStr);
+
+        listAdapter = new SimpleCursorAdapter(this, R.layout.search_list_item, messageResultCursor,
+                fromColumn, toView, 0);
+
+        setListAdapter(listAdapter);
         return true;
     }
 
