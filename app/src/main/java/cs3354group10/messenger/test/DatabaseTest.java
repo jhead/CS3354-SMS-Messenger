83b5fe604eb3ced576f3f3a6d17501311979d38a
Justin Head <justin@cubedhost.com>
2015-12-07 20:24:49 -0600

    Initial DB test class

--- app/src/main/java/cs3354group10/messenger/test/DatabaseTest.java
+++ app/src/main/java/cs3354group10/messenger/test/DatabaseTest.java
@@ -1,7 +1,32 @@
 package cs3354group10.messenger.test;
 
-/**
- * Created by justin on 12/7/15.
- */
-public class DatabaseTest {
+import android.test.AndroidTestCase;
+
+import cs3354group10.messenger.Contact;
+import cs3354group10.messenger.Message;
+import cs3354group10.messenger.MessageState;
+import cs3354group10.messenger.db.MessageDatabase;
+
+public class DatabaseTest extends AndroidTestCase {
+
+    protected Contact testContact;
+
+    protected void setUp() {
+        testContact = new Contact("testContactName");
+
+    }
+
+    protected void tearDown() {
+        //
+    }
+
+    public void testInsertSentMessage() {
+        Message testMessage = new Message(testContact, "Test Message Text", MessageState.SENT);
+
+        long result = MessageDatabase.insertMessage(getContext(), testMessage);
+
+        // SQLite insert error
+        assertNotSame("SQLite error occurred", result, -1);
+    }
+
 }
