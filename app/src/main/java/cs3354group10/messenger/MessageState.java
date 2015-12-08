fb0ef5b2fc308bdf0d5a264bae4ece73a1ba155f
Justin Head <justin@cubedhost.com>
2015-12-07 22:07:16 -0600

    Add docs for Contact, Message, and MessageState

--- app/src/main/java/cs3354group10/messenger/MessageState.java
+++ app/src/main/java/cs3354group10/messenger/MessageState.java
@@ -1,5 +1,8 @@
 package cs3354group10.messenger;
 
+/**
+ * Represents the state of a message in terms of being sent, received, or a draft.
+ */
 public enum MessageState {
 
     // A "sent" message is one that the user sent to someone else.
---
+++ app/src/main/java/cs3354group10/messenger/test/ThreadListActivityTest.java
@@ -0,0 +1,7 @@
+package cs3354group10.messenger.test;
+
+/**
+ * Created by justin on 12/7/15.
+ */
+public class ThreadListActivityTest {
+}
